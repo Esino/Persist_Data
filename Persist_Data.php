bbc7a92c89ac952cb7e1c5d1d267a738ee46f154
System Administrator <root@Pauls-MBP.default>
2017-10-06 13:54:21 +0100

    {I made few changes in the "processform" file and "persist_data" to make it work better}

--- Persist_Data.php
+++ Persist_Data.php
@@ -1,13 +1,15 @@
 <?php 
 
-	function Connect(){
-		$servername = "localhost";
-		$username = "root";
-		$password = "";
-		$dbname = "postDB";
-	
-		$conn = new mysqli($servername, $username, $password, $dbname) or die("Connection failed:" . $conn->$connect_error);
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
 
-		return $conn;
-	}
-	?>
+	// Create connection
+	$conn = new mysqli($servername, $username, $password);
+
+	// Check connection
+	if ($conn->connect_error) {
+	    die("Connection failed: " . $conn->connect_error);
+	} 
+		echo "Connected successfully";
+?>
--- processform.php
+++ processform.php
@@ -1,8 +1,7 @@
 <?php
-	require 'Persist_Data.php';
+	/*require 'Persist_Data.php';
 	$conn = Connect();
-	$name = $conn->real_escape_string($_POST['Name']);
-	$comment = $conn->real_escape_string($_POST['Comment']);
+	
 	$query = "INSERT into myComment (Name, Comment) VALUES ('" . $name . "','" . $comment . "')";
 	$success = $conn->query($query);
 
@@ -15,5 +14,55 @@
 
 	echo "Thank you for your comment <br>";
 
-	$conn->close();
+	$conn->close(); */
+	$name = $conn->real_escape_string($_POST['Name']);
+	$comment = $conn->real_escape_string($_POST['Comment']);
+
+
+	class Db{
+		private $server;
+		private $username;
+		private $password;
+		private $database;
+		private $conn;
+
+		public function __construct(){
+			$this->server = 'localhost';
+			$this->username = 'root';
+			$this->password = '';
+			$this->database = 'postDB'; // You can call it whatever
+
+			if(!$this->isConnectionValid()){
+				die('Connection is not valid');
+			}else{
+				$this->conn = new mysqli($this->server, $this->username, $this->password);
+			}
+		}
+
+		public function isConnectionValid(){
+			$conn = new mysqli($this->server, $this->username, $this->password);
+
+			// Check connection
+			if ($conn->connect_error){
+			    return false;
+			}
+
+			return true;
+		}
+
+		public function insert($sql){
+			if ($this->conn->query($sql) === TRUE) {
+			    echo "New record created successfully";
+			} else {
+			    echo "Error: " . $sql . "<br>" . $conn->error;
+			}
+
+			$conn->close();
+		}
+	}
+
+	$db = new Db();
+	$sql = "INSERT INTO myComment (`Name`, `Comment`) VALUES ('" . $name . "','" . $comment . "')";
+	$db->insert($sql);
+
 ?>
